66b7df0ac916f0ffdb0bd2786917cff381804ae5
David Sims <simsdj82@gmail.com>
2024-06-20 20:07:23 +1000

    Unit test updates
    Signed-off-by: David Sims <simsdj82@gmail.com>

--- cmd/tofu/main_test.go
+++ cmd/tofu/main_test.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -291,7 +292,7 @@ func (c *testCommandCLI) Help() string     { return "" }
 
 func TestWarnOutput(t *testing.T) {
 	mock := cli.NewMockUi()
-	wrapped := &ui{mock}
+	wrapped := &ui{Ui: mock}
 	wrapped.Warn("WARNING")
 
 	stderr := mock.ErrorWriter.String()
@@ -367,4 +368,36 @@ func TestMkConfigDir_noparent(t *testing.T) {
 	if err.Error() != expectedError {
 		t.Fatalf("Expected error: %s, but got: %v", expectedError, err)
 	}
+}
+
+func TestGetGlobalOptions(t *testing.T) {
+	testCases := []struct {
+		name     string
+		args     []string
+		expected map[string]string
+	}{
+		{"positive tc chdir", []string{"-chdir=target", "plan", "-state=file.tfstate"}, map[string]string{"chdir": "target"}},
+		{"positive tc 1 version", []string{"-version", "plan", "-state=file.tfstate"}, map[string]string{"version": ""}},
+		{"positive tc 2 version", []string{"--version", "plan", "-state=file.tfstate"}, map[string]string{"version": ""}},
+		{"positive tc random", []string{"-random", "plan", "-state=file.tfstate"}, map[string]string{"random": ""}},
+		{"positive tc multi", []string{"-help", "-version", "plan", "-state=file.tfstate"}, map[string]string{"help": "", "version": ""}},
+		{"positive tc omitted", []string{"plan", "-state=file.tfstate"}, map[string]string{}},
+	}
+
+	var opts map[string]string
+	var err error
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			opts, err = getGlobalOptions(tc.args)
+			assert.EqualValues(t, tc.expected, opts)
+			assert.Nil(t, err)
+		})
+	}
+
+	t.Run("negative tc chdir", func(t *testing.T) {
+		opts, err = getGlobalOptions([]string{"-chdir", "plan", "-state=file.tfstate"})
+		assert.Nil(t, opts)
+		assert.Error(t, err)
+	})
 }
